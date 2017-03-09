3a5e5c8fa600075d3e6a39f42dd3a5193fd237f9
Armin <armin.ag@outlook.com>
2017-03-09 20:21:14 +0100

    Create Add Event Form

---
+++ asp.net-mvc-event-manager/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using asp.net_mvc_event_manager.Models;
+using asp.net_mvc_event_manager.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace asp.net_mvc_event_manager.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ asp.net-mvc-event-manager/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using asp.net_mvc_event_manager.Models;
+using System.Collections.Generic;
+
+namespace asp.net_mvc_event_manager.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int GenreId { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
