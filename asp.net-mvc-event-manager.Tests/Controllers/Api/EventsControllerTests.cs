2cbda07235c42558ed6a49c5fc11aab37cfe9321
Armin <armin.ag@outlook.com>
2017-03-11 20:06:48 +0100

    Add Unit Tests for Events Api

---
+++ asp.net-mvc-event-manager.Tests/Controllers/Api/EventsControllerTests.cs
@@ -0,0 +1,72 @@
+﻿using asp.net_mvc_event_manager.Controllers.Api;
+using asp.net_mvc_event_manager.Core;
+using asp.net_mvc_event_manager.Core.Models;
+using asp.net_mvc_event_manager.Core.Repositories;
+using asp.net_mvc_event_manager.Tests.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace asp.net_mvc_event_manager.Tests.Controllers.Api
+{
+    [TestClass]
+    public class EventsControllerTests
+    {
+        private EventsController _controller;
+        private Mock<IEventRepository> _mockRepository;
+        private string _userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IEventRepository>();
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.SetupGet(u => u.Events).Returns(_mockRepository.Object);
+
+            _controller = new EventsController(mockUnitOfWork.Object);
+            _userId = "1";
+            _controller.MockCurrentUser(_userId, "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Cancel_NoEventWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.Cancel(1);
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [TestMethod]
+        public void Cancel_EventIsCanceled_ShouldReturnNotFound()
+        {
+            var newEvent = new Event();
+            newEvent.Cancel();
+
+            _mockRepository.Setup(r => r.GetEventWithAttendees(1)).Returns(newEvent);
+
+            var result = _controller.Cancel(1);
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [TestMethod]
+        public void Cancel_UserCancelingAnotherUsersEvent_ShouldReturnUnauthorized()
+        {
+            var currentEvent = new Event { ArtistId = _userId + "-" };
+            _mockRepository.Setup(r => r.GetEventWithAttendees(1)).Returns(currentEvent);
+
+            var result = _controller.Cancel(1);
+            result.Should().BeOfType<UnauthorizedResult>();
+        }
+
+        [TestMethod]
+        public void Cancel_ValidRequest_ShouldReturnOk()
+        {
+            var currentEvent = new Event { ArtistId = _userId };
+            _mockRepository.Setup(r => r.GetEventWithAttendees(1)).Returns(currentEvent);
+
+            var result = _controller.Cancel(1);
+            result.Should().BeOfType<OkResult>();
+        }
+    }
+}
---
+++ asp.net-mvc-event-manager.Tests/Extensions/ApiControllerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Web.Http;
+
+namespace asp.net_mvc_event_manager.Tests.Extensions
+{
+    public static class ApiControllerExtensions
+    {
+        public static void MockCurrentUser(this ApiController controller, string userId, string username)
+        {
+            var identity = new GenericIdentity(username);
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name", username));
+            identity.AddClaim(new Claim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", userId));
+
+            var principal = new GenericPrincipal(identity, null);
+
+            // Install-Package Microsoft.AspNet.WebApi.Core
+            controller.User = principal;
+        }
+    }
+}
