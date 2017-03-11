fdc655126b6a5b980e4feea2232e4e881bafc698
Armin <armin.ag@outlook.com>
2017-03-11 19:14:44 +0100

    Add Unit Test Project with Moq and FluentAssertions

---
+++ asp.net-mvc-event-manager.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace asp.net_mvc_event_manager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
