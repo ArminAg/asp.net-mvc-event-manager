67e1c593005387f4e4562cac4024eb4754d55326
Armin <armin.ag@outlook.com>
2017-03-11 23:50:19 +0100

    Add Unit Tests for Attendances Api

---
+++ asp.net-mvc-event-manager.Tests/Controllers/Api/AttendancesControllerTests.cs
@@ -0,0 +1,83 @@
+﻿using asp.net_mvc_event_manager.Controllers.Api;
+using asp.net_mvc_event_manager.Core;
+using asp.net_mvc_event_manager.Core.Dtos;
+using asp.net_mvc_event_manager.Core.Models;
+using asp.net_mvc_event_manager.Core.Repositories;
+using asp.net_mvc_event_manager.Tests.Extensions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Web.Http.Results;
+
+namespace asp.net_mvc_event_manager.Tests.Controllers.Api
+{
+    [TestClass]
+    public class AttendancesControllerTests
+    {
+        private AttendancesController _controller;
+        private Mock<IAttendanceRepository> _mockRepository;
+        private string _userId;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockRepository = new Mock<IAttendanceRepository>();
+
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.SetupGet(u => u.Attendances).Returns(_mockRepository.Object);
+
+            _controller = new AttendancesController(mockUnitOfWork.Object);
+            _userId = "1";
+            _controller.MockCurrentUser(_userId, "user1@domain.com");
+        }
+
+        [TestMethod]
+        public void Attend_UserAttendingEventForWhichHeHasAnAttendance_ShouldReturnBadRequest()
+        {
+            var attendance = new Attendance();
+            _mockRepository.Setup(r => r.GetAttendance(1, _userId)).Returns(attendance);
+
+            var result = _controller.Attend(new AttendanceDto { EventId = 1 });
+
+            result.Should().BeOfType<BadRequestErrorMessageResult>();
+        }
+
+        [TestMethod]
+        public void Attend_ValidRequest_ShouldReturnOk()
+        {
+            var result = _controller.Attend(new AttendanceDto { EventId = 1 });
+
+            result.Should().BeOfType<OkResult>();
+        }
+
+        [TestMethod]
+        public void DeleteAttendance_NoAttendanceWithGivenIdExists_ShouldReturnNotFound()
+        {
+            var result = _controller.DeleteAttendance(1);
+
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [TestMethod]
+        public void DeleteAttendance_ValidRequest_ShouldReturnOk()
+        {
+            var attendance = new Attendance();
+            _mockRepository.Setup(r => r.GetAttendance(1, _userId)).Returns(attendance);
+
+            var result = _controller.DeleteAttendance(1);
+
+            result.Should().BeOfType<OkNegotiatedContentResult<int>>();
+        }
+
+        [TestMethod]
+        public void DeleteAttendance_ValidRequest_ShouldReturnTheIdOfDeletedAttendance()
+        {
+            var attendance = new Attendance();
+            _mockRepository.Setup(r => r.GetAttendance(1, _userId)).Returns(attendance);
+
+            var result = (OkNegotiatedContentResult<int>)_controller.DeleteAttendance(1);
+
+            result.Content.Should().Be(1);
+        }
+    }
+}
