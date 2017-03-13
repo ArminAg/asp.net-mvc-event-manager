0bb886cd2c3fec42e483a1f7c7935c1f37b5d844
Armin <armin.ag@outlook.com>
2017-03-13 17:18:08 +0100

    Add Tests for Domain Models

---
+++ asp.net-mvc-event-manager.Tests/Domain/Models/EventTests.cs
@@ -0,0 +1,34 @@
+﻿using asp.net_mvc_event_manager.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace asp.net_mvc_event_manager.Tests.Domain.Models
+{
+    [TestClass]
+    public class EventTests
+    {
+        [TestMethod]
+        public void Cancel_WhenCalled_ShouldSetIsCanceledToTrue()
+        {
+            var newEvent = new Event();
+
+            newEvent.Cancel();
+
+            newEvent.IsCanceled.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Cancel_WhenCalled_EachAttendeeShouldHaveNotification()
+        {
+            var newEvent = new Event();
+            newEvent.Attendances.Add(new Attendance { Attendee = new ApplicationUser { Id = "1" } });
+
+            newEvent.Cancel();
+
+            // TODO: This could be pushed into the Event class (eg event.GetAttendees())
+            var attendees = newEvent.Attendances.Select(a => a.Attendee).ToList();
+            attendees[0].UserNotifications.Count.Should().Be(1);
+        }
+    }
+}
---
+++ asp.net-mvc-event-manager.Tests/Domain/Models/NotificationTests.cs
@@ -0,0 +1,28 @@
+﻿using asp.net_mvc_event_manager.Core.Models;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace asp.net_mvc_event_manager.Tests.Domain.Models
+{
+    [TestClass]
+    public class NotificationTests
+    {
+        [TestMethod]
+        public void EventCanceled_WhenCalled_ShouldReturnedANotificationForCanceledEvent()
+        {
+            var newEvent = new Event();
+            var notification = Notification.EventCanceled(newEvent);
+
+            // Again, here, we have two assertions, but that doesn't mean we're
+            // violating the single responsibility principle. We're verifying 
+            // one logical fact: that upon calling Notification.EventCanceled()
+            // we'll get a notification object for the canceled event. This notification
+            // object should be in the right state, meaning its type should be
+            // EventCanceled and its event should be the event for each we created 
+            // the notification. 
+
+            notification.Type.Should().Be(NotificationType.EventCanceled);
+            notification.Event.Should().Be(newEvent);
+        }
+    }
+}
